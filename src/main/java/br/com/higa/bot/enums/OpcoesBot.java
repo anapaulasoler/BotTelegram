6d7e0c51fc08eca7d57ec8823e85b7c53c539d31
anapaulasoler <anapaula_soller@yahoo.com.br>
2021-06-23 14:43:06 -0300

    Reademe included

--- src/main/java/br/com/higa/bot/enums/OpcoesBot.java
+++ src/main/java/br/com/higa/bot/enums/OpcoesBot.java
@@ -2,9 +2,9 @@ package br.com.higa.bot.enums;
 
 public enum OpcoesBot {
     CEP("/cep", "envie '/cep <cep_desejado>' para receber as informacoes sobre o local consultado."),
-    BOLETOS_EM_ABERTO("/boletos_em_aberto", "Retorna boletos em aberto"),
-    OPT_02("/opt02", "desc deste servico."),
-    OPT_03("/opt03", "desc deste servico.");
+    BOLETOS_EM_ABERTO("/boletos_em_aberto", "Retorna boletos em aberto");
+    //OPT_02("/opt02", "desc deste servico."),
+    //OPT_03("/opt03", "desc deste servico.");
 
     private String nome;
     private String descServico;
